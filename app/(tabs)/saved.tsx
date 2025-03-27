50ffa5bec51e750da73ee4d9e358a2e280ab0dae
denostta <denostaanthony08@gmail.com>
2025-03-27 08:15:23 +0300

    2nd commit

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Saved = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">Saved</Text>
+    </View>
+  );
+};
+
+export default Saved;
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Search = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">Search</Text>
+    </View>
+  );
+};
+
+export default Search;
